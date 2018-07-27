215d20dd677796dd4aee6c32df460c1b3f7e5492
JamesKinler <james193722@yahoo.com>
2018-07-26 21:43:23 -0700

    updated the website

--- js/myjs.js
+++ js/myjs.js
@@ -36,12 +36,54 @@ new ScrollMagic.Scene({
     // .setPin('.counter')
     .on('enter', function(e){
       setTimeout(function(){
-    odometer.innerHTML = 3000;
+    odometer.innerHTML = 14558;
       }, 1000)
     })
 
 
     .addTo(controller);
+
+    new ScrollMagic.Scene({
+          triggerElement: '.third_header',
+          // triggerHook: 'onBottom',
+          offset: 10,
+      })
+      // .addIndicators()
+      // .setPin('.counter')
+      .on('enter', function(e){
+        setTimeout(function(){
+      $('#odometer2').html(12134);
+        }, 1000)
+      })
+
+
+      .addTo(controller);
+
+      new ScrollMagic.Scene({
+        triggerElement: ".fourthsection",
+        triggerHook: "onLeave"
+      })
+      .setPin(".fourthsection")
+      .addTo(controller);
+
+
+      $('.raindrop').ripples({
+			resolution: 512,
+			dropRadius: 10, //px
+			perturbance: 0.01,
+
+		});
+
+	setInterval(function() {
+		var $el = $('.raindrop');
+		var x = Math.random() * $el.outerWidth();
+		var y = Math.random() * $el.outerHeight();
+		var dropRadius = 20;
+		var strength = 0.08 + Math.random() * 0.08;
+		$el.ripples('drop', x, y, dropRadius, strength);
+	}, 400);;
+
+
 
 
 
